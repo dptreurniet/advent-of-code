63bf3b553ba9b24f1e8527af819513a0a831396b
Daan Treurniet <dptreurniet@gmail.com>
2024-12-04 08:46:18 +0100

    update 2024-01

--- year_2024/day01.py
+++ year_2024/day01.py
@@ -8,7 +8,7 @@ def solve(data):
     A = sorted([line[0] for line in lines])
     B = sorted([line[1] for line in lines])
     C = [abs(A[i] - B[i]) for i in range(len(A))]
-    print(f'Answer part 1: {sum(C)}')
-    print(f'Answer part 2: {sum([a * B.count(a) for a in A])}')
+    yield sum(C)
+    yield sum([a * B.count(a) for a in A])
 
 
